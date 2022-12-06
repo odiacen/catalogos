510baf8c280b6f45dbe6ff16efcb230820e7a39a
odiacen <96943520+odiacen@users.noreply.github.com>
2022-12-06 08:04:09 -0500

    Fix_Variations

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,8 @@ export default function Home(productos: any) {
     }
   })
 
+
+
   return (
     <>   
      
@@ -32,7 +34,8 @@ export default function Home(productos: any) {
                         <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
                             <Image src={producto.images[0].src} alt={''} width={60} height={60} />
                             <span className="flex items-center">{producto.name}</span>
-                            <span className="flex items-center">${producto.price}
+                            <span className="flex items-center">
+                            <div  dangerouslySetInnerHTML={{__html: producto.price_html}}/>                            
                             <svg data-accordion-icon className="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </span>
                         </button>
--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -8,39 +8,45 @@ const variation = (product) => {
     const variations = product['product']
     let isArray = Array.isArray(variations)
     let prod = []
+    let showName = []
     
     if (isArray) {
       variations.forEach(function( item, index) {
-        if (item.stock_quantity > 0)
+        if (item.stock_quantity > 0) {
+          if (item.attributes.length > 1) {
+            let name = []
+            item.attributes.forEach(function( option, index) {
+                name.push(option.option)
+            })
+            showName.push(name.toString().replace(",", "-"))
+            
+          }  
           prod.push(item)
+        }
       })  
     }
     
-    
-       
     return (
-      <div>
-        {prod.map((producto) => (
-<div key={prod.id} className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <Image src={producto.image.src} alt={''} width={800} height={800} priority={true}/>
-    <div className="px-5 pb-5">
-        <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{producto.attributes[0].option}</h5>
-        </a>
-        <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">{producto.stock_quantity} Disponibles</span>
-            </div>
-        <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white"> Precio: ${producto.price}</span>
-            </div>
-    </div>
-</div>
-    ))}  
-
-              
-    </div>
-
-    )
+              <div>
+                  {prod.map((producto, index) => (
+                      
+                      <div key={producto.id} className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                          <Image src={producto.image.src} alt={''} width={800} height={800} priority={true}/>
+                          <div className="px-5 pb-5">
+                              <a>
+                                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{producto.attributes.length > 1 ? showName[index] : producto.attributes[0].option}</h5>
+                              </a>
+                              <div className="flex items-center justify-between">
+                                  <span className="text-3xl font-bold text-gray-900 dark:text-white">{producto.stock_quantity} Disponibles</span>
+                                  </div>
+                              <div className="flex items-center justify-between">
+                                  <span className="text-3xl font-bold text-gray-900 dark:text-white"> Precio: ${producto.price}</span>
+                                  </div>
+                          </div>
+                      </div>
+                  ))}  
+              </div>
+            )
 }
 
 export default variation
@@ -61,7 +67,7 @@ export async function getStaticPaths(context) {
 export async function getStaticProps (context) {
     
     const prodId = context.params.id
-    const res = await fetch(`https://mercadeocubano.com/wp-json/wc/v3/products/${prodId}/variations?per_page=50&consumer_key=ck_42999e7fed435f74ce7e09872c8ee0fd643137ea&consumer_secret=cs_979a8d2977025ed95f62de200c6293f3818f863c`)
+    const res = await fetch(`https://mercadeocubano.com/wp-json/wc/v3/products/${prodId}/variations?per_page=100&consumer_key=ck_42999e7fed435f74ce7e09872c8ee0fd643137ea&consumer_secret=cs_979a8d2977025ed95f62de200c6293f3818f863c`)
     let product = await res.json()
     
     return {
